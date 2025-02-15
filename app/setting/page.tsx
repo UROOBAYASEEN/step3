5436e795d66942b3a0b61c5bfb40390308f9f6f9
UROOBAYASEEN <uroobanawal74@gmail.com>
2025-02-15 10:46:32 +0500

    ye coment

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -15,140 +15,8 @@ import { toast } from "react-toastify"
 import { BiSolidPencil } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import { IoIosLogOut } from "react-icons/io";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
  
-const AlertDialog = AlertDialogPrimitive.Root
- 
-const AlertDialogTrigger = AlertDialogPrimitive.Trigger
- 
-const AlertDialogPortal = AlertDialogPrimitive.Portal
- 
-const AlertDialogOverlay = React.forwardRef<
-  React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Overlay
-    className={cn(
-      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className
-    )}
-    {...props}
-    ref={ref}
-  />
-))
-AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName
- 
-const AlertDialogContent = React.forwardRef<
-  React.ElementRef<typeof AlertDialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <AlertDialogPortal>
-    <AlertDialogOverlay />
-    <AlertDialogPrimitive.Content
-      ref={ref}
-      className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-        className
-      )}
-      {...props}
-    />
-  </AlertDialogPortal>
-))
-AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName
- 
-const AlertDialogHeader = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col space-y-2 text-center sm:text-left",
-      className
-    )}
-    {...props}
-  />
-)
-AlertDialogHeader.displayName = "AlertDialogHeader"
- 
-const AlertDialogFooter = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-      className
-    )}
-    {...props}
-  />
-)
-AlertDialogFooter.displayName = "AlertDialogFooter"
- 
-const AlertDialogTitle = React.forwardRef<
-  React.ElementRef<typeof AlertDialogPrimitive.Title>,
-  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Title>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Title
-    ref={ref}
-    className={cn("text-lg font-semibold", className)}
-    {...props}
-  />
-))
-AlertDialogTitle.displayName = AlertDialogPrimitive.Title.displayName
- 
-const AlertDialogDescription = React.forwardRef<
-  React.ElementRef<typeof AlertDialogPrimitive.Description>,
-  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Description>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Description
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props}
-  />
-))
-AlertDialogDescription.displayName =
-  AlertDialogPrimitive.Description.displayName
- 
-const AlertDialogAction = React.forwardRef<
-  React.ElementRef<typeof AlertDialogPrimitive.Action>,
-  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Action
-    ref={ref}
-    className={cn(buttonVariants(), className)}
-    {...props}
-  />
-))
-AlertDialogAction.displayName = AlertDialogPrimitive.Action.displayName
- 
-const AlertDialogCancel = React.forwardRef<
-  React.ElementRef<typeof AlertDialogPrimitive.Cancel>,
-  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Cancel>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Cancel
-    ref={ref}
-    className={cn(
-      buttonVariants({ variant: "outline" }),
-      "mt-2 sm:mt-0",
-      className
-    )}
-    {...props}
-  />
-))
-AlertDialogCancel.displayName = AlertDialogPrimitive.Cancel.displayName
- 
-export {
-  AlertDialog,
-  AlertDialogPortal,
-  AlertDialogOverlay,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogFooter,
-  AlertDialogTitle,
-  AlertDialogDescription,
-  AlertDialogAction,
-  AlertDialogCancel,
-}
 
 
 
@@ -245,7 +113,8 @@ const Page = () => {
      
      <form>
      <AlertDialogHeader>
-        <AlertDialogTitle><h1 className="font-bold text-2xl ">Please Update Your Name</h1></AlertDialogTitle>
+        <AlertDialogTitle className="font-bold text-2xl "> 
+           Please Update Your Name</AlertDialogTitle>
         <AlertDialogDescription>
           <form>
             <input type="text" placeholder="Enter First Name" value={firstname} onChange={(e)=>setfirstname(e.target.value)} className="w-full border border-black font-bold mt-3 py-3"/>
@@ -279,7 +148,7 @@ const Page = () => {
      
      <form>
      <AlertDialogHeader>
-        <AlertDialogTitle><h1 className="font-bold text-2xl ">Please Update YourEmale</h1></AlertDialogTitle>
+        <AlertDialogTitle className="font-bold text-2xl ">Please Update YourEmale</AlertDialogTitle>
         <AlertDialogDescription>
           <form>
             <input type="email" placeholder="Enter Email" value={emaiil} onChange={(e)=>setemaiil(e.target.value)} className="w-full border border-black font-bold mt-3 py-3 mb-5"/>
@@ -313,7 +182,7 @@ const Page = () => {
      
      <form>
      <AlertDialogHeader>
-        <AlertDialogTitle><h1 className="font-bold text-2xl ">Please Update Passward</h1></AlertDialogTitle>
+        <AlertDialogTitle className="font-bold text-2xl ">Please Update Passward</AlertDialogTitle>
         <AlertDialogDescription>
           <form>
             <input type="email" placeholder="Enter Passward" value={passward} onChange={(e)=>setpassward(e.target.value)} className="w-full border border-black font-bold mt-3 py-3 mb-5"/>
@@ -342,7 +211,7 @@ const Page = () => {
      
      <form>
      <AlertDialogHeader>
-        <AlertDialogTitle><h1 className="font-bold text-2xl ">Do You Want to logout Your Account</h1></AlertDialogTitle>
+        <AlertDialogTitle className="font-bold text-2xl ">Do You Want to logout Your Account</AlertDialogTitle>
         <AlertDialogDescription>
           <form>
            
@@ -375,7 +244,7 @@ const Page = () => {
      
      <form>
      <AlertDialogHeader>
-        <AlertDialogTitle><h1 className="font-bold text-2xl text-red-700 ">Do you want to delete your account  </h1></AlertDialogTitle>
+        <AlertDialogTitle  className="font-bold text-2xl text-red-700 ">Do you want to delete your account</AlertDialogTitle>
         <AlertDialogDescription>
           <form>
            
